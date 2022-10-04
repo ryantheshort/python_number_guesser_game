e3695004b93f9723cd92d3fc67f11f11045c40b2
Ryan Short <shorttryan@gmail.com>
2022-10-04 02:23:40 -0400

    completes level 2

---
+++ level_two.py
@@ -0,0 +1,27 @@
+from random import randint
+import time
+
+prompt = input("Pick a number between 1 and 10 for the computer to guess. ")
+number = int(prompt)
+
+guesses = 0
+
+while guesses < 3:
+    computer_guess = randint(1, 10)
+    print('Computer guesses', computer_guess)
+
+    guesses = guesses + 1
+
+    if computer_guess == number:
+        break
+    elif computer_guess < number:
+        print('Too low!')
+    elif computer_guess > number:
+        print('Too high!')
+
+    time.sleep(1)
+
+if computer_guess == number:
+    print('The computer has guessed your number!')
+elif computer_guess != number:
+    print('The computer has been bested!')
